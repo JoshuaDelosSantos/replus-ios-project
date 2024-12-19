b5d5a48c2cff50d69ebe4bde36728c797ea8cd1b
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-12-19 21:28:25 +1000

    Add comments

--- replusGoBackEnd/cmd/server/main.go
+++ replusGoBackEnd/cmd/server/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+    "fmt"  // Package for formatted I/O
+    "log"  // Package for logging
+    "net/http"  // Package for HTTP client and server implementations
 
-	"github.com/JoshuaDelosSantos/replus-ios-project/replus-backend/internal/config"
+	"github.com/JoshuaDelosSantos/replus-ios-project/replus-backend/internal/config"  // Custom package for configuration
 )
 
 func main() {
+    // Load configuration settings
     cfg := config.LoadConfig()
 
+    // Print a message indicating the app is starting and on which port
     fmt.Printf("Starting app on port %s\n", cfg.AppPort)
+	
+    // Define a handler function for the root URL path
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Welcome to Replus App!")
+        fmt.Fprintln(w, "Welcome to Replus App!")  // Send a welcome message as the HTTP response
     })
 	
-    // Here, you can pass the DB config to your database connection
+    // Start the HTTP server on the specified port and log any errors
     log.Fatal(http.ListenAndServe(":"+cfg.AppPort, nil))
 }
