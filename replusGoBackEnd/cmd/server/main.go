b9bb5738ee9663bb625cccdd66b2ceb0bef8628c
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-12-19 13:25:32 +1000

    Refactor to use config

--- replusGoBackEnd/cmd/server/main.go
+++ replusGoBackEnd/cmd/server/main.go
@@ -4,13 +4,15 @@ import (
     "fmt"
     "log"
     "net/http"
+
+	"github.com/JoshuaDelosSantos/replus-ios-project/replus-backend/internal/config"
 )
 
 func main() {
-    http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "API is healthy!")
-    })
+    cfg := config.LoadConfig()
 
-    log.Println("Starting server on :8080...")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    fmt.Printf("Starting app on port %s\n", cfg.AppPort)
+	
+    // Here, you can pass the DB config to your database connection
+    log.Fatal(http.ListenAndServe(":"+cfg.AppPort, nil))
 }
