9114a2f607be9def414e316317ed0ea0c9e0c14e
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-12-23 10:58:48 +1000

    Add code for repo layer db file

---
+++ replusGoBackEnd/internal/repository/db.go
@@ -0,0 +1,9 @@
+package repository
+
+import "database/sql"
+
+type DB interface {
+    Query(query string, args ...interface{}) (*sql.Rows, error)
+    QueryRow(query string, args ...interface{}) *sql.Row
+    Exec(query string, args ...interface{}) (sql.Result, error)
+}
