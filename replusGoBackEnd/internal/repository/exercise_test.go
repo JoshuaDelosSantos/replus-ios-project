572096555953c65c4edc35cb4fb2184435d28b42
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-12-25 09:49:01 +1000

    Add mock db setup function test

---
+++ replusGoBackEnd/internal/repository/exercise_test.go
@@ -0,0 +1,19 @@
+package repository
+
+import (
+	"fmt"
+	"log"
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/JoshuaDelosSantos/replus-ios-project/replus-backend/internal/models"
+	"github.com/JoshuaDelosSantos/replus-ios-project/replus-backend/internal/testutil"
+)
+
+func setupMockExerciseDB(t *testing.T) (ExerciseRepository, sqlmock.Sqlmock) {
+	db, mock := testutil.NewMockDB(t)
+	// Use the constructor for consistency
+	log.Println("Mock DB and repository setup complete.")
+	repo := NewExerciseRepository(db)
+	return repo, mock
+}
