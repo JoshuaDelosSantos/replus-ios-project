cc9bda34441614ad2db8bceebfe7e06a969eaf6f
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-12-25 09:15:22 +1000

    Add interface definition

--- replusGoBackEnd/internal/repository/exercise.go
+++ replusGoBackEnd/internal/repository/exercise.go
@@ -1,1 +1,17 @@
 package repository
+
+
+import (
+	"fmt"
+	"github.com/JoshuaDelosSantos/replus-ios-project/replus-backend/internal/models"
+)
+
+// ExerciseRepository defines the contract for exercise database operations.
+type ExerciseRepository interface {
+	GetExercises() ([]models.Exercise, error)
+	CreateExercise(exercise models.Exercise) (models.Exercise, error)
+	GetExercisesBySessionID(sessionID int) ([]models.Exercise, error)
+	UpdateExercise(exercise models.Exercise) error
+	DeleteExercise(exerciseID int) error
+}
+
