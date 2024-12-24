a086181db6dc9f629e4479941d2b80346cf02e31
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-12-24 10:22:45 +1000

    Add log statement

--- replusGoBackEnd/internal/testutil/dbutil.go
+++ replusGoBackEnd/internal/testutil/dbutil.go
@@ -4,9 +4,11 @@ import (
     "testing"
     "database/sql"
     "github.com/DATA-DOG/go-sqlmock"
+	"log"
 )
 
 func NewMockDB(t *testing.T) (*sql.DB, sqlmock.Sqlmock) {
+	log.Println("Setting up mock DB and repository...")
     db, mock, err := sqlmock.New()
     if err != nil {
         t.Fatalf("Error creating mock DB: %v", err)
