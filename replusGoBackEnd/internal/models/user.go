e0b3e1c95eed2661c827c78cdfc3c1aa903d6feb
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2024-12-30 09:43:09 +1000

    Add features for user authentication

--- replusGoBackEnd/internal/models/user.go
+++ replusGoBackEnd/internal/models/user.go
@@ -3,4 +3,6 @@ package models
 type User struct {
     ID       int    `json:"user_id"`
     UserName string `json:"user_name"`
+    Email    string `json:"email"`
+    Password string `json:"password,omitempty"`
 }
